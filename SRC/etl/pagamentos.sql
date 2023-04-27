61d19a62056ad4b844bb93e2ef2df28d1104518c
caio-calo <caio-calo>
2023-04-27 19:51:18 +0000

    Pagamentos ETL Final

--- SRC/etl/pagamentos.sql
+++ SRC/etl/pagamentos.sql
@@ -11,6 +11,7 @@ ON t1.idPedido = t2.idPedido
 LEFT JOIN silver.olist.item_pedido as t3
 ON t1.idPedido = t3.idPedido
 
+--intervalo de observação
 WHERE t1.dtPedido < '2018-01-01'
 AND t1.dtPedido >= add_months('2018-01-01', -6)
 ),
@@ -74,4 +75,3 @@ FROM tb_pagamentos as t1
 
 LEFT JOIN tb_cartao as t2
 ON t1.idVendedor = t2.idVendedor
-
